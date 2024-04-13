4c51612619c845a42bf39470a34aecc58852a658
CiottiGiorgio <gciotti.dev@gmail.com>
2024-04-13 19:35:00 +0200

    More granular tradeoff involving participants and winners. Still under the same safety assumptions and both are still uint8.

--- projects/verifiable-giveaway-contracts/smart_contracts/verifiable_giveaway/contract.py
+++ projects/verifiable-giveaway-contracts/smart_contracts/verifiable_giveaway/contract.py
@@ -32,12 +32,14 @@ from lib_pcg import pcg128_init, pcg128_random
 # log2(#possible_prn_sequences) - log2(#possible_k_permutations) >= 128
 # log2(#possible_k_permutations) <= log2(2^256) - 128
 # log2(#possible_k_permutations) <= 128
-# Given the fact that
-# log2(80!/(80-20)!) ~ 122,694 < 128
-# We conclude that any possible commitment involving at most 80 participants and 20 winners is safe to
+
+# We are going to set more than one pair of safety parameters to allow the user to tradeoff participants for winners.
+# log2(255!/(255-16)!) ~ 127,217
+# log2(80!/(80-20)!)   ~ 122,694
+# log2(47!/(47-25)!)   ~ 127,435
+# log2(35!/(35-30)!)   ~ 126,018
+# We conclude that any possible commitment involving at participants and winners at most these parameters is safe to
 # compute given our entropy level (256 bits of vrf output).
-SAFETY_MAX_PARTICIPANTS = 80
-SAFETY_MAX_WINNERS = 20
 
 
 class VerifiableGiveaway(ARC4Contract):
@@ -58,8 +60,15 @@ class VerifiableGiveaway(ARC4Contract):
     ) -> None:
         assert Global.round <= block.native - TemplateVar[UInt64]("SAFETY_ROUND_GAP")
 
-        assert 1 <= winners.native <= SAFETY_MAX_WINNERS
-        assert 2 <= participants.native <= SAFETY_MAX_PARTICIPANTS
+        assert 1 <= winners.native
+        assert 2 <= participants.native
+        assert (
+            # We don't need to actually check that participants is <= 255 because participants is arc4.UInt8
+            (winners.native <= 16) or
+            (participants.native <= 80 and winners.native <= 20) or
+            (participants.native <= 47 and winners.native <= 25) or
+            (participants.native <= 35 and winners.native <= 30)
+        )
         assert winners.native <= participants.native
 
         # FIXME: It would be best to use a struct so that we have easier decoding off-chain.
@@ -101,9 +110,24 @@ class VerifiableGiveaway(ARC4Contract):
         # We use a "truncated" version of the algorithm where we stop after "winners" iterations.
         # The array to be shuffled is an array with the numbers from 1 to "participants".
         # Since we have constrained "participants", we can populate the array in constant time
-        #  by slicing a pre-computed bytearray with numbers from 1 to SAFETY_MAX_PARTICIPANTS.
+        #  by slicing a pre-computed bytearray with numbers from 1 to 255 (arc4.UInt8 max value).
         participants = Bytes.from_hex(
-            "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f50"
+            "0102030405060708090a0b0c0d0e0f10"
+            "1112131415161718191a1b1c1d1e1f20"
+            "2122232425262728292a2b2c2d2e2f30"
+            "3132333435363738393a3b3c3d3e3f40"
+            "4142434445464748494a4b4c4d4e4f50"
+            "5152535455565758595a5b5c5d5e5f60"
+            "6162636465666768696a6b6c6d6e6f70"
+            "7172737475767778797a7b7c7d7e7f80"
+            "8182838485868788898a8b8c8d8e8f90"
+            "9192939495969798999a9b9c9d9e9fa0"
+            "a1a2a3a4a5a6a7a8a9aaabacadaeafb0"
+            "b1b2b3b4b5b6b7b8b9babbbcbdbebfc0"
+            "c1c2c3c4c5c6c7c8c9cacbcccdcecfd0"
+            "d1d2d3d4d5d6d7d8d9dadbdcdddedfe0"
+            "e1e2e3e4e5e6e7e8e9eaebecedeeeff0"
+            "f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
         )[: committed_participants.native]
         # We want to stop after "winners" iterations unless "winners" == "participants"
         #  in which case we want to stop at "participants" - 1.
