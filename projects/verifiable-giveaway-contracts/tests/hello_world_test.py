6a075fb85937884cf50614758e71abe31d043889
CiottiGiorgio <gciotti.dev@gmail.com>
2024-09-02 17:52:30 +0200

    WIP: Adding the default unit testing file.

--- projects/verifiable-giveaway-contracts/tests/hello_world_test.py
+++ projects/verifiable-giveaway-contracts/tests/hello_world_test.py
@@ -1,9 +1,12 @@
 from collections.abc import Iterator
+from hashlib import sha3_256
+from typing import Literal
 
+import algopy
 import pytest
-from algopy_testing import AlgopyTestContext, algopy_testing_context
+from algopy_testing import AlgopyTestContext, algopy_testing_context, arc4_prefix
 
-from smart_contracts.hello_world.contract import HelloWorld
+from smart_contracts.verifiable_giveaway.contract import Commitment, VerifiableGiveaway
 
 
 @pytest.fixture()
@@ -12,13 +15,26 @@ def context() -> Iterator[AlgopyTestContext]:
         yield ctx
 
 
-def test_hello(context: AlgopyTestContext) -> None:
+def test_reveal(context: AlgopyTestContext) -> None:
     # Arrange
-    dummy_input = context.any.string(length=10)
-    contract = HelloWorld()
+    dummy_randomness_beacon = context.any.application(
+        logs=[arc4_prefix(sha3_256(b"NOT-SO-RANDOM-DATA").digest())]
+    )
+
+    context.set_template_var("RANDOMNESS_BEACON_ID", dummy_randomness_beacon.id)
+
+    contract = VerifiableGiveaway()
+    contract.commitment[context.default_sender] = Commitment(
+        tx_id=algopy.arc4.StaticArray[algopy.arc4.Byte, Literal[32]].from_bytes(
+            context.any.bytes(32)
+        ),
+        round=algopy.arc4.UInt64(0),
+        participants=algopy.arc4.UInt32(2),
+        winners=algopy.arc4.UInt8(2),
+    )
 
     # Act
-    output = contract.hello(dummy_input)
+    output = contract.reveal()
 
     # Assert
-    assert output == f"Hello, {dummy_input}"
+    # assert output == f"Hello, {dummy_input}"
