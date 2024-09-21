f1e0714fe5b6f78dfa28fb10a41c53415f1ff275
CiottiGiorgio <gciotti.dev@gmail.com>
2024-09-22 00:56:57 +0200

    fix shuffle modal title.

--- projects/verifiable-shuffle-frontend/src/components/Shuffle.tsx
+++ projects/verifiable-shuffle-frontend/src/components/Shuffle.tsx
@@ -118,7 +118,7 @@ const Shuffle = ({ openModal, setModalState }: ShuffleInterface) => {
   return (
     <dialog id="transact_modal" className={`modal ${openModal ? 'modal-open' : ''} bg-slate-200`}>
       <form method="dialog" className="modal-box">
-        <h3 className="font-bold text-lg">Send payment transaction</h3>
+        <h3 className="font-bold text-lg">Send Shuffle Application Call</h3>
         <br />
         <input
           type="number"
