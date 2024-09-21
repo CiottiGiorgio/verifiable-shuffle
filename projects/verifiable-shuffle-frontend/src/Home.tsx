54523f41298cae53b1a8c9cb235ba923d04ebb7d
CiottiGiorgio <gciotti.dev@gmail.com>
2024-09-22 00:44:08 +0200

    resume app name and disclaimer.

--- projects/verifiable-shuffle-frontend/src/Home.tsx
+++ projects/verifiable-shuffle-frontend/src/Home.tsx
@@ -15,7 +15,7 @@ const Home: React.FC<HomeProps> = () => {
     setOpenWalletModal(!openWalletModal)
   }
 
-  const toggleDemoModal = () => {
+  const toggleShuffleModal = () => {
     setOpenShuffleModal(!openShuffleModal)
   }
 
@@ -24,32 +24,36 @@ const Home: React.FC<HomeProps> = () => {
       <div className="hero-content text-center rounded-lg p-6 max-w-md bg-white mx-auto">
         <div className="max-w-md">
           <h1 className="text-4xl">
-            Welcome to <div className="font-bold">AlgoKit 🙂</div>
+            Welcome to <div className="font-bold">Verifiable Shuffle</div>
           </h1>
-          <p className="py-6">
-            This starter has been generated using official AlgoKit React template. Refer to the resource below for next steps.
-          </p>
 
           <div className="grid">
-            <a
-              data-test-id="getting-started"
-              className="btn btn-primary m-2"
-              target="_blank"
-              href="https://github.com/algorandfoundation/algokit-cli"
-            >
-              Getting started
-            </a>
-
             <div className="divider" />
             <button data-test-id="connect-wallet" className="btn m-2" onClick={toggleWalletModal}>
               Wallet Connection
             </button>
 
             {activeAddress && (
-              <button data-test-id="transactions-demo" className="btn m-2" onClick={toggleDemoModal}>
-                Transactions Demo
+              <button data-test-id="application-call" className="btn m-2" onClick={toggleShuffleModal}>
+                Send Application Call
               </button>
             )}
+            <div className="divider" />
+            <h1 className="text-2xl">Disclaimer</h1>
+            <p className="py-6">
+              This software is distributed under{' '}
+              <a href="https://github.com/CiottiGiorgio/verifiable-shuffle/blob/3e541254403a4299823f633d28e5f4a05758905b/LICENSE">
+                MIT License
+              </a>
+              .<br />
+              <br />
+              In addition to the conditions of use of this software, <a href="https://github.com/CiottiGiorgio">the author</a> declares that
+              this software is for demonstration purposes only.
+              <br />
+              <br />
+              While great effort in research, implementation, and testing was put into this project, the author makes no guarantees on the
+              quality of the statistical properties resulting from the use of this software.
+            </p>
           </div>
 
           <ConnectWallet openModal={openWalletModal} closeModal={toggleWalletModal} />
