4e32202a3bbe46a44cb0b2aa5b2ccb3fd938ce5c
CiottiGiorgio <giorgio.ciotti.dev@proton.me>
2026-03-09 21:49:25 +0100

    adding the reveal workflow

--- projects/verifiable-shuffle-frontend/src/routes/+page.svelte
+++ projects/verifiable-shuffle-frontend/src/routes/+page.svelte
@@ -16,25 +16,62 @@
 	algorandClient.setDefaultSigner(transactionSigner);
 
 	const handleCommit = async () => {
+		if (!activeAddress.current) {
+			alert('Please connect your wallet first.');
+			return;
+		}
+
 		const shuffleFactory = new VerifiableShuffleFactory({
 			algorand: algorandClient,
-			defaultSender: activeAddress.current ?? undefined
+			defaultSender: activeAddress.current
 		});
 		const shuffleClient = await shuffleFactory.getAppClientByCreatorAndName({
 			creatorAddress: PUBLIC_APP_CREATOR
 		});
 
-		const simResult = await shuffleClient
-			.newGroup()
-			.optIn.commit({
-				args: { delay: 1, participants: 5, winners: 3 },
-				extraFee: microAlgos(10_000)
+		let isOptedIn: boolean;
+		try {
+			isOptedIn = true;
+			await shuffleClient.state.local(activeAddress.current).getAll();
+		} catch (error) {
+			isOptedIn = false;
+		}
+
+		const onCompleteCommitGroup = !isOptedIn
+			? shuffleClient.newGroup().optIn
+			: shuffleClient.newGroup();
+		await onCompleteCommitGroup
+			.commit({
+				args: { delay: 1, participants: 2, winners: 1 },
+				maxFee: microAlgos(10_000)
 			})
-			.simulate({ allowUnnamedResources: true });
-		console.log(simResult);
+			.send({ populateAppCallResources: true, coverAppCallInnerTransactionFees: true });
+	};
+
+	const handleReveal = async () => {
+		if (!activeAddress.current) {
+			alert('Please connect your wallet first.');
+			return;
+		}
+
+		const shuffleFactory = new VerifiableShuffleFactory({
+			algorand: algorandClient,
+			defaultSender: activeAddress.current
+		});
+		const shuffleClient = await shuffleFactory.getAppClientByCreatorAndName({
+			creatorAddress: PUBLIC_APP_CREATOR
+		});
+
+		const randomness = await shuffleClient
+			.newGroup()
+			.closeOut.reveal({ args: {}, maxFee: microAlgos(10_000) })
+			.send({ populateAppCallResources: true, coverAppCallInnerTransactionFees: true });
+
+		console.log(randomness.returns[0]?.winners);
 	};
 </script>
 
 <div>
 	<button onclick={handleCommit}>Commit</button>
+	<button onclick={handleReveal}>Reveal</button>
 </div>
